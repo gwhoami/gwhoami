1544070eeeca707532cdc866016031fab9bb2846
gwhoami <gwhoamihelp@gmail.com>
2022-05-13 16:39:55 -0400

    Update register.js
    see the out put of register page

--- react-gwhoami/src/container/register.js
+++ react-gwhoami/src/container/register.js
@@ -71,19 +71,22 @@ const RegisterUser = React.memo(() => {
                         <div><h1 className="mb-8 text-3xl">Registration</h1></div>
                         <div class="flex ...">
                             <div class="w-1/2 ... ">
-                                <InputText styleClass="flex flex-col mb-4" formKey="name" formRef={regRef} uiRefresh={ui} label="Name" placeholder="Name" required="Name is required" />
+                            <InputText styleClass="flex flex-col mb-4" formKey="First Name" formRef={regRef} uiRefresh={ui} label="First Name" placeholder="First Name" />
+                                
                             </div>
                             <div class="w-1/6 ... "></div>
                             <div class="w-1/2 ... ">
-                                <InputEmail styleClass="flex flex-col mb-4" formKey="username" formRef={regRef} uiRefresh={ui} label="Email" placeholder="Email" required="Email is required" /></div>
+                            <InputEmail styleClass="flex flex-col mb-4" formKey="Last Name" formRef={regRef} uiRefresh={ui} label="Last Name" placeholder="Last Name" /></div>
+                                
                         </div>
                         <div class="flex ...">
                             <div class="w-1/2 ... ">
-                                <InputText styleClass="flex flex-col mb-4" formKey="First Name" formRef={regRef} uiRefresh={ui} label="First Name" placeholder="First Name" />
+                            <InputText styleClass="flex flex-col mb-4" formKey="name" formRef={regRef} uiRefresh={ui} label="Name" placeholder="Name" required="Name is required" />
                             </div>
                             <div class="w-1/6 ... "></div>
                             <div class="w-1/2 ... ">
-                                <InputEmail styleClass="flex flex-col mb-4" formKey="Last Name" formRef={regRef} uiRefresh={ui} label="Last Name" placeholder="Last Name" /></div>
+                            <InputEmail styleClass="flex flex-col mb-4" formKey="username" formRef={regRef} uiRefresh={ui} label="Email" placeholder="Email" required="Email is required" /></div>  
+
                         </div>
                         <div class="flex ...">
                             <PasswordCheck styleClass="flex flex-col mb-4" formKey="password" ui={ui} formRef={regRef} uiRefresh={ui} />
