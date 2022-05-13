0e5f883faa353f51f20667de419476287ef1d6f1
gwhoami <gwhoamihelp@gmail.com>
2022-05-13 16:35:10 -0400

    Update index.js
    password separation

--- react-gwhoami/src/component/forms/index.js
+++ react-gwhoami/src/component/forms/index.js
@@ -180,10 +180,11 @@ export const PasswordCheck = React.memo(({styleClass, formKey, formRef, ui, ID="
                 {isNotValid() && <div className='flex justify-start items-center text-red-500 text-xs mt-1'>Password is required</div>}
                 {formRef.current[formKey].length > 0 && <PasswordStrengthBar password={formRef.current[formKey]} className="mt-2" onChangeScore={(score, feed)=>scoreFeed(score, feed)}/>}
             </div>
+            <div class="flex ..."></div>
             <div class="w-1/6 ... "></div>
-                            <div class="w-1/2 ... "></div>
             <div className={`flex flex-col mb-4${comparePass() ? ' mark-err' : ''}`}>
                 <label className="text-gray-600 mb-1 required">Re-Password</label>
+               
                 <input 
                     type="password" 
                     className={`border ${comparePass() ? 'border-red-500' : 'border-gray-400'} w-full p-2 rounded focus:border-dodge-b`} 
@@ -193,6 +194,7 @@ export const PasswordCheck = React.memo(({styleClass, formKey, formRef, ui, ID="
                 />
                 {comparePass() && <div className='flex justify-start items-center text-red-500 text-xs mt-1'>Password not matched</div>}
             </div>
+            
         </>
     );
 });
