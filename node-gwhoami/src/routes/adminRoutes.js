8a42843d362b5af39440b1082d1150ec27d72385
gwhoami <gwhoamihelp@gmail.com>
2022-05-19 15:42:07 -0400

    change
    test

--- node-gwhoami/src/routes/adminRoutes.js
+++ node-gwhoami/src/routes/adminRoutes.js
@@ -1,50 +1,50 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const sgMail = require('@sendgrid/mail');
-const { Users, UserBasicInfo, userBasicInfoSchema, PasswordChangeList} = require("../models/adminUsersModels");
+const { Users, UserBasicInfo, userBasicInfoSchema, PasswordChangeList } = require("../models/adminUsersModels");
 const router = express.Router();
 
-router.get('/adminlogincheck', async(req, res)=>{
-     const result = await Users.findOne({username: req.query.username, password: req.query.password});
-     if (!result) res.status(500).json({success: false});
+router.get('/adminlogincheck', async (req, res) => {
+     const result = await Users.findOne({ username: req.query.username, password: req.query.password });
+     if (!result) res.status(500).json({ success: false });
      else res.send(result);
 });
 
-router.get('/logincheck', async(req, res)=>{
-     const result = await UserBasicInfo.findOne({username: req.query.username, password: req.query.password});
-     if (!result) res.status(500).json({success: false});
+router.get('/logincheck', async (req, res) => {
+     const result = await UserBasicInfo.findOne({ username: req.query.username, password: req.query.password });
+     if (!result) res.status(500).json({ success: false });
      else res.send(result);
 });
 
-router.post('/userbasicreg', async(req, res) => {
+router.post('/userbasicreg', async (req, res) => {
      const id = require('mongodb').ObjectId;
-     const info = {...req.body, _id: new id()}
-     new UserBasicInfo(info).save((err, output)=>{
-          if (err) res.status(500).json({success: false, message: err.message});
-          else res.send({success: true});
+     const info = { ...req.body, _id: new id() }
+     new UserBasicInfo(info).save((err, output) => {
+          if (err) res.status(500).json({ success: false, message: err.message });
+          else res.send({ success: true });
      });
 });
-router.post('/userbasicupdate', async(req, res)=>{
+router.post('/userbasicupdate', async (req, res) => {
      const _id = req.body.id;
      const record = JSON.parse(req.body.data);
      record.no_email_check = true;
-     await UserBasicInfo.findByIdAndUpdate(_id, {...record});
-     res.send({success: true});
+     await UserBasicInfo.findByIdAndUpdate(_id, { ...record });
+     res.send({ success: true });
 });
-router.get('/userinfo', async(req, res)=>{
+router.get('/userinfo', async (req, res) => {
      const result = await UserBasicInfo.find();
      res.send(result);
 });
-router.get('/removeuser', async(req, res)=>{
+router.get('/removeuser', async (req, res) => {
      await UserBasicInfo.findByIdAndRemove(req.query.id);
-     res.send({success: true});
+     res.send({ success: true });
 });
-router.post('/verifyemail', async(req, res)=> {
+router.post('/verifyemail', async (req, res) => {
      try {
-          const result = await UserBasicInfo.findOne({username: req.body.username});
-          if (!result) res.json({success: false});
+          const result = await UserBasicInfo.findOne({ username: req.body.username });
+          if (!result) res.json({ success: false });
           else {
-               const results = await new PasswordChangeList({email: req.body.username}).save();
+               const results = await new PasswordChangeList({ email: req.body.username }).save();
                const ID = results._id;
                const URL = `${decodeURIComponent(req.body.changeURL)}/${ID}`;
                const msg = {
@@ -56,35 +56,35 @@ router.post('/verifyemail', async(req, res)=> {
                }
                sgMail.setApiKey(process.env['EMAIL_API']);
                await sgMail.send(msg);
-               res.json({success: true});
+               res.json({ success: true });
           }
      } catch (err) {
-          res.status(500).json({success: false, message: err.message});
+          res.status(500).json({ success: false, message: err.message });
      }
-     
+
 });
 
-router.get('/checkusername', async(req, res)=>{
+router.get('/checkusername', async (req, res) => {
      try {
           const result = await PasswordChangeList.findById(req.query.id);
-          if (!result) res.json({success: false});
+          if (!result) res.json({ success: false });
           else {
-               const user = await UserBasicInfo.findOne({username: result._doc.email});
-               if (!user) res.json({success: false});
-               else res.send({success: true,userid: user._doc._id, ...result._doc});
+               const user = await UserBasicInfo.findOne({ username: result._doc.email });
+               if (!user) res.json({ success: false });
+               else res.send({ success: true, userid: user._doc._id, ...result._doc });
           }
      } catch (err) {
-          res.status(500).json({success: false, message: err.message});
+          res.status(500).json({ success: false, message: err.message });
      }
 });
 
-router.get('/changepassword', async(req, res)=>{
+router.get('/changepassword', async (req, res) => {
      try {
-          const result = await UserBasicInfo.findByIdAndUpdate(req.query.userid, {password: req.query.password});
+          const result = await UserBasicInfo.findByIdAndUpdate(req.query.userid, { password: req.query.password });
           await PasswordChangeList.findByIdAndRemove(req.query.id);
-          res.json({success: true});
-     } catch(err) {
-          res.status(500).json({success: false, message: err.message});
+          res.json({ success: true });
+     } catch (err) {
+          res.status(500).json({ success: false, message: err.message });
      }
 });
 
